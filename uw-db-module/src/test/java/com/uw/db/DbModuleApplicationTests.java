f73e3f33892bfea565266bfc916c0535cb5b3df0
ashutosh <asingh2030@gmail.com>
2020-04-06 00:25:28 +0530

    Underwriting DB module base structure.

---
+++ uw-db-module/src/test/java/com/uw/db/DbModuleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uw.db;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbModuleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
